d6cac1392272f09778ecfb3f9295cd96fcd9e883
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2024-07-04 11:30:37 -0500

    Update update_database.py

--- src/update_database.py
+++ src/update_database.py
@@ -5,7 +5,7 @@ def update_database(txt1, txt2):
   user='staffwizardmysqladmin'
   pwd='b5&8$smb7$J9BMKK'
   host='azmysql-staff-wizard-prd-01.mysql.database.azure.com'
-  db='iknow-ku'
+  db='mysql'
   
   cnx = mysql.connector.connect(user=user, 
                               password=pwd,
